b9e99acbaf7ee8b1c4107c74c1bce965e89f5c55
noa <do@cre-noa.me>
2019-02-12 19:17:22 +0900

    phpからサンプルデータをJSONで返す状態

--- WWW/output.php
+++ WWW/output.php
@@ -1,7 +1,53 @@
 <?php
   $reqid = $_GET[q];
 
-  echo "$reqid";
+echo '
 
+{
+    "intoroTalk":[
+        {
+            "dataType":"talk",
+            "who":"Aさん",
+            "content":"セリフ"
+        },
+        {
+            "dataType":"img",
+            "who":"Bさん",
+            "content":"imagePath"
+        }
+    ],
+    "choice":{
+        "buttonList":[
+            {
+                "choiceContent":"選択肢１内容",
+                "judge":false
+            },
+            {
+                "choiceContent":"選択肢２内容",
+                "judge":true
+            }
+        ],
+        "html":"html tag here"
+    },
+    "answerTalk":[
+        {
+            "dataType":"talk",
+            "who":"Aさん",
+            "content":"セリフ"
+        },
+        {
+            "dataType":"img",
+            "who":"Bさん",
+            "content":"imagePath"
+        }
+    ],
+    "description":[
+        "説明文段落１",
+        "説明文段落２"
+    ]
+}
+
+
+';
 
  ?>
