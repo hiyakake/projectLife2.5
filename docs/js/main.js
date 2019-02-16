e329dfd79834c08b1b951bb344aeb6c0f4755d57
LavP <lavp@nextlav.xyz>
2019-02-16 17:58:32 +0900

    フッダーと次の問題に行く処理を実装

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,20 +2,41 @@ new Vue({
     el:'#vm',
     data:{
         quesNo:1, //問題進行番号
+        quesRank:'1', //問題の難易度を、ユーザーのランクを管理 A=0 B=1 C=2
         json:null, //QuesAPI
         choiceSelect:null, //どの選択肢を選択したかを保持,
-        screenSwitch:'hello' //画面遷移
+        screenSwitch:'ques', //画面遷移
+        quesLog:{ //選択履歴保持
+            quesID:[],
+            result:[],
+            quesRank:[]
+        }
     },
     methods:{
         getQuesJson:function(){
             //テストサーバーから取得
-            //const URL = 'https://dev1.cre-noa.me/quesAPI.php?q='+(this.quesNo*100)
+            //const URL = 'https://dev1.cre-noa.me/quesAPI.php?r='+(this.quesRank)
             //サンプルデータから取得
             const URL = 'api/ques1.json'
             axios
                 .get(URL)
                 .then(response => (this.json = response.data))
-        } 
+        },
+        nextQues:function(){
+            //次の問題のランクを決定
+            if(this.choiceSelect == true) this.quesRank++
+            else this.quesRank--
+            if(this.quesRank > 2) this.quesRank = 2
+            if(this.quesRank < 0) this.quesRank = 0
+            //結果格納
+            this.quesLog.quesID[this.quesNo-1] = this.json.quesID
+            this.quesLog.result[this.quesNo-1] = this.choiceSelect
+            this.quesLog.quesRank[this.quesNo-1] = this.quesRank
+            //次の問題への手続き
+            this.quesNo++
+            this.choiceSelect = null
+            window.scrollTo(0,0)
+        }
     },
     mounted:function(){
         this.getQuesJson()
